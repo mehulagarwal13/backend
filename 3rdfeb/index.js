be0c40eeee91c1d9ed8f0fc9f2c26830cf4961a3
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-03 21:15:58 +0530

    SERVER

---
+++ 3rdfeb/index.js
@@ -0,0 +1,8 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    res.end("Hello from 3rdfeb folder");
+    // console.log("Request URL:", req.url);
+});
+server.listen(4000,()=>{
+    console.log("server is listening on port 4000");
+});
