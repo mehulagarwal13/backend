66c24cece14afae0d784563db1be40e5be36b03c
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-04 01:23:31 +0530

    w

--- 3rdfeb/index.js
+++ 3rdfeb/index.js
@@ -1,6 +1,15 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    res.end("Hello from 3rdfeb folder");
+    if(req.url==="/"){
+        res.end("Welcome to Home Page");
+    } else if(req.url==="/about"){
+        res.end("Welcome to About Page");
+    } else if(req.url==="/contact"){
+        res.end("Welcome to Contact Page");
+    } else{
+        res.end("Page Not Found");
+    }
+    //res.end("Hello from 3rdfeb folder");
     // console.log("Request URL:", req.url);
 });
 server.listen(4000,()=>{
