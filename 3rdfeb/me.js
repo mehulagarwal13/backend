2d95b2e35380c7c21c2e98921154dcc7fda51bdf
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-03 07:02:24 +0530

    readFile("filename","utf-8",(err,data)=>{})

--- 3rdfeb/me.js
+++ 3rdfeb/me.js
@@ -1,8 +1,20 @@
 console.log("this is me.js file in 3rdfeb folder");
-
+const fs=require('fs');
 function sum(a,b){
     return a+b;
 }
+fs.readFile("./data.json","utf-8",(err,data)=>{
+    console.log(data);
+})
+
+fs.readFile("./data.json","utf-8",(err,data)=>{
+    if(err) return console.log("error",err);
+    const obj=JSON.parse(data);
+    console.log(Object.keys(obj));
+    console.log(Object.values(obj));
+    console.log(Object.entries(obj));
+})
+
 setTimeout(()=>{
     console.log("hello after 5 second");
 },5000)
