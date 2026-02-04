ac8e127b9547051a5e30e0c94a0d753646427619
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-04 16:02:06 +0530

    id

--- 4thfeb/index.js
+++ 4thfeb/index.js
@@ -1,13 +1,24 @@
 const express=require('express');
 const app=express();
-app.use((req,res)=>{
+app.use(/^\/c+t$/,(req,res)=>{
+    res.end("Hey daksh i am plus");  //    /^\/c?t  
+});
+app.use("/cont/:id",(req,res)=>{
+    console.log(req.params);
+    res.end("Hey daksh ");  //    /^\/c?t  
+});
+app.use("/",(req,res)=>{
     if(req.url=='/contact'){
         res.send("I am your contact page");
+    }
+    else if(req.url==='/ct'){
+        res.send("hey daksh is there ");
     }
     else{
          res.send("Hey Mehul Agarawal this side How are you");       //server banana express ki help sa
     }
 });
+
 app.listen(5000,()=>{
     console.log("Server is listening on port 5000");
 });
