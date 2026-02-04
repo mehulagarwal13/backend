074e3c3d7d826e5e118979e5c90ca9a217d2d9a2
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-04 15:03:47 +0530

    server

--- 4thfeb/index.js
+++ 4thfeb/index.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const app=express();
+app.use((req,res)=>{
+    res.send("Hey Mehul Agarawal");       //server banana express ki help sa
+});
 app.listen(5000,()=>{
     console.log("Server is listening on port 5000");
 });
