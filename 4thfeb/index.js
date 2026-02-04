4ccb0689a16d62b4e1bb883125806a7032ba3da8
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-04 15:22:08 +0530

    yes

--- 4thfeb/index.js
+++ 4thfeb/index.js
@@ -1,7 +1,12 @@
 const express=require('express');
 const app=express();
 app.use((req,res)=>{
-    res.send("Hey Mehul Agarawal");       //server banana express ki help sa
+    if(req.url=='/contact'){
+        res.send("I am your contact page");
+    }
+    else{
+         res.send("Hey Mehul Agarawal this side How are you");       //server banana express ki help sa
+    }
 });
 app.listen(5000,()=>{
     console.log("Server is listening on port 5000");
