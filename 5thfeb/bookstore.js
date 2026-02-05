27478c9e11cbbf809a2ce897f24209107375c3ac
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-05 23:12:19 +0530

    bookstore

---
+++ 5thfeb/bookstore.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const bookstore=[
+    {id:1,name:"the rebel",author:"Mehul"},
+    {id:2,name:"the conflict",author:"Daksh"},
+    {id:3,name:"major",author:"prakash"}
+]
+
+app.get("/book",(req,res)=>{
+    res.send(bookstore);
+})
+app.get("/book/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const b=bookstore.find(info=>info.id===id);
+    res.send(b);
+})
+app.post("/po",(req,res)=>{
+
+    console.log("send the data");
+});
+app.listen(3000,()=>{
+    console.log("the server is running at port 3000");
+});
