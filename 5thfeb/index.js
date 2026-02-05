f34465bd47e72b5ddbf61f7746914a6a22e1acce
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-05 19:30:10 +0530

    parse

--- 5thfeb/index.js
+++ 5thfeb/index.js
@@ -1,8 +1,14 @@
 const express=require("express");
 const app=express();
-app.use("/user",(req,res)=>{
-    
-})
-app.listen(6000,()=>{
-    console.log("server is running at port number 6000")
+app.use(express.json());  //data backend ma lena ka liya parsing karani padhti hai
+app.get("/user",(req,res)=>{
+    res.send({name:"MEHUL",age:21,college:"JIIT"});
+});
+app.post("/user", (req, res) => {
+    console.log(req.body);
+    res.send("DATA saved success");
+});
+
+app.listen(1000,()=>{
+    console.log("server is running at port number 1000")
 });
