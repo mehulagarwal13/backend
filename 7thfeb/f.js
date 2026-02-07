d2715ef366bac44ffee509ffe193cb494dcd5afa
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-07 19:56:28 +0530

    middleware

---
+++ 7thfeb/f.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+
+app.use("/user",(req,res,next)=>{
+    console.log(`${Date.now()} ${req.method} ${req.originalUrl}`);//yeh ek middleware ka example hai 
+    //authorisation bhi asani sa kar sakta hai
+    next();
+})
+
+app.post("/user",(req,res)=>{
+    res.send("data saved");
+})
+
+app.get("/user",(req,res)=>{
+    res.send("data get");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("data delete");
+})
+
+app.listen(3000,()=>{
+    console.log("listen at 3000")
+})
--- 7thfeb/index.js
+++ 7thfeb/index.js
@@ -4,22 +4,39 @@ app.use(express.json());
 
 
 
-//route handler hai yeh
-app.use("/send",(req,res,next)=>{
+//route handler hai yeh   inko ma alag alag bhi likh sakta hu
+app.use("/send",(req,res,next)=>{ //middleware 
     console.log("I am First");
     next();
     console.log("sixth");
 },
 (req,res,next)=>{
-    console.log("I am Second");
+    console.log("I am Second"); //middleware kuki response last wala call back se mil rha hai
     next();
     console.log("fifth");
 },
 (req,res)=>{
-    console.log("i am Third");
-    res.send("hey I can implement multipeple call back by help of next")
+    console.log("i am Third"); 
+    res.send("hey I can implement multipeple call back by help of next")//request sender
     console.log("fourth");
-})
+}) //yeh nicha jo code hai same work karega upar jaisa 
+
+// app.use("/send",(req,res,next)=>{    
+//     console.log("I am First");
+//     next();
+//     console.log("sixth");
+// })
+// app.use("/send",(req,res,next)=>{
+//     console.log("I am Second");
+//     next();
+//     console.log("fifth");
+// })
+
+// app.use("/send",(req,res)=>{
+//     console.log("i am Third");
+//     res.send("hey I can implement multipeple call back by help of next")
+//     console.log("fourth");
+// })
 
 app.listen(3000,()=>{
     console.log("app is running at port 3000")
