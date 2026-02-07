9716687494c2e208c31a8d00dae86974e0e9b675
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-07 19:11:10 +0530

    next function

---
+++ 7thfeb/index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+
+app.use("/send",(req,res,next)=>{
+    console.log("I am First");
+    next();
+},(req,res,next)=>{
+    console.log("I am Second");
+    next();
+},(req,res)=>{
+    console.log("i am Third");
+    res.send("hey I can implement multipeple call back by help of next")
+})
+
+app.listen(3000,()=>{
+    console.log("app is running at port 3000")
+})
