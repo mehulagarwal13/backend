297183d120b33a1505d7335a9b4be4412eb659b0
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-07 19:16:50 +0530

    ok

--- 7thfeb/index.js
+++ 7thfeb/index.js
@@ -2,17 +2,23 @@ const express=require("express");
 const app=express();
 app.use(express.json());
 
+
+
+//route handler hai yeh
 app.use("/send",(req,res,next)=>{
     console.log("I am First");
     next();
+    console.log("sixth");
 },
 (req,res,next)=>{
     console.log("I am Second");
     next();
+    console.log("fifth");
 },
 (req,res)=>{
     console.log("i am Third");
     res.send("hey I can implement multipeple call back by help of next")
+    console.log("fourth");
 })
 
 app.listen(3000,()=>{
