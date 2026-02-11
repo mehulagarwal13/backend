57567d1535fea1b2c5c9e26c4ca39eafd6f2c8c1
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-11 17:49:28 +0530

    bycrypt password

--- feb11/f.js
+++ feb11/f.js
@@ -13,8 +13,11 @@ async function Hashing() {
         console.timeEnd("hash");
         console.log(salt)
         console.log(hashpass);
-        
-
+        //compare kaisa karta hoga jaisa login time ma hmara db ma store hua hoga hashpass
+        //so usko usse compare karata hai usme version+salt+orginalpass 
+        //toh salt phle enterd pass ma add hoga then dono ko compare karega
+        const matchit=await bcrypt.compare(password,hashpass);
+        console.log(matchit);
     } 
     catch (err) {
         console.error(err);
