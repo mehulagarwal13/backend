349920fd042b0bf07ca28631afadc37e2ea5b6e6
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-13 09:46:25 +0530

    new commit

---
+++ feb13/index.js
@@ -0,0 +1,57 @@
+const main=require("./database");
+const express=require("express");
+const app=express();
+app.use(express.json());
+const user = require("./user");
+const validatoruser=require("./validator")
+const bcrypt=require("bcrypt")
+app.post("/register",async(req,res)=>{
+    //api check karunga db alling sa phle    
+    try{
+        validatoruser(req.body);
+        req.body.password=await bcrypt.hash(req.body.password,10); 
+        const data=await user.create(req.body);
+        res.send({message:"send the data",
+             data: data})
+    }
+    catch(err){
+        res.send("ERROR " + err.message)
+    }
+})
+app.post("/login", async (req, res) => {
+    try {
+        const a = await user.findById(req.body._id);
+
+        if (!a) {
+            return res.send("User not found");
+        }
+
+        if (req.body.email !== a.email) {
+            return res.send("Invalid credentials");
+        }
+
+        const p = await bcrypt.compare(req.body.password, a.password);
+
+        if (!p) {
+            return res.send("Invalid credentials");
+        }
+
+        res.send("login success");
+    }
+    catch (err) {
+        res.send(err.message);
+    }
+});
+
+main()
+.then(async()=>{
+    console.log("DB IS CONNECTED")
+    app.listen(3000,()=>{
+        console.log("listen at 3000")
+    })
+ })
+ .catch((err)=>{
+    console.log(err)
+ })
+
+ 
