6c7a94ed1fe543193c73b51fd6696224ef0023ef
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-19 19:19:29 +0530

    redis

---
+++ feb17/index.js
@@ -0,0 +1,112 @@
+const main=require("./database");
+const express=require("express");
+const app=express()
+const user = require("./user");
+const validatoruser=require("./validator")
+const bcrypt=require("bcrypt")
+const cookieparse=require("cookie-parser")
+const jwt = require('jsonwebtoken');
+const auth=require("./middleware/auth");
+const { now } = require("mongoose");
+app.use(cookieparse());
+require("dotenv").config({path:"../.env"}); 
+app.use(express.json())
+app.post("/logout",async(req,res)=>{
+    try{
+    res.cookie("token",null,{expires:new Date(Date.now())})
+    res.send("logout ")
+    }
+    catch(err){
+       res.send("ERROR " + err.message)
+    }
+})
+
+app.post("/register",async(req,res)=>{
+    //api check karunga db alling sa phle    
+    try{
+        validatoruser(req.body);
+        req.body.password=await bcrypt.hash(req.body.password,10); 
+        const data=await user.create(req.body);
+        res.send({message:"send the data",
+             data: data})
+    }
+    catch(err){
+        res.send("ERROR " + err.message)
+    }
+})
+app.post("/login", async (req, res) => {
+    try {
+        const a = await user.findOne({email:req.body.email});
+
+        if (!a) {
+            return res.send("User not found");
+        }
+
+        // if (req.body.email !== a.email) {
+        //     return res.send("Invalid credentials");
+        // }
+        const p = await bcrypt.compare(req.body.password, a.password);
+
+        if (!p) {
+            return res.send("Invalid credentials");
+        }
+        //jwt token bhejo cokkies ki help sa
+
+        const token = jwt.sign(
+               { id: a._id, email: a.email },   // payload object
+                 process.env.SECRET_KEY,                   // secret key
+                { expiresIn: "1d" }              // optional
+        );
+
+        res.cookie("token",token);
+        res.send("login success");
+    }
+    catch (err) {
+        res.send(err.message);
+    }
+});
+
+//
+// app.get("/info",async(req,res)=>{
+//     try {
+//         //mujhe verify karna hoga ki yeh user wahi hai jisne login kia
+//          const playload=jwt.verify(req.cookies.token,"mysecretkey") //jo token hai apka cokkie ma aur secretkey
+//          //agar yeh verify nhi ho pata toh yeh error fek kar marega
+//          console.log(playload);
+//          const result=await user.findById(playload.id);
+//          console.log(req.cookies);
+//          res.send(result);
+//     }
+//     catch (err) {
+//         res.send(err.message);
+//     }
+// })
+app.get("/info",auth, async (req, res) => {
+    try {
+        // const token = req.cookies.token;
+        // if (!token) {
+        //     throw new Error("Token not found");
+        // }
+        // const playload = jwt.verify(token, "mysecretkey");
+        // console.log(playload);
+        // const result = await user.findById(playload.id);
+
+        res.send(req.user);
+    }
+    catch (err) {
+        res.send(err.message);
+    }
+});
+
+main()
+.then(async()=>{
+    console.log("DB IS CONNECTED")
+    app.listen(process.env.PORT,()=>{
+        console.log("listen at 3000")
+    })
+ })
+ .catch((err)=>{
+    console.log(err)
+ })
+
+ 
