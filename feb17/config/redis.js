b77e5b9452fa82733f2176526b9390e2a7aef936
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-19 23:04:42 +0530

    new commit

--- feb17/config/redis.js
+++ feb17/config/redis.js
@@ -1,2 +1,7 @@
 const redis=require("redis");
-const client=redis.createClient({})
+const client=redis.createClient({username: 'default',
+    password: 'pZSnzZkkus0mQdxQEoEksaXbEXXmYmVf',
+    socket: {
+        host: 'redis-16734.crce276.ap-south-1-3.ec2.cloud.redislabs.com',
+        port: 16734
+    }});
