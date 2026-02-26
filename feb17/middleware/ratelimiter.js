f85267ef04b209b4075e23b6551b8115c37fe5ef
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-26 11:27:14 +0530

    throw new error

--- feb17/middleware/ratelimiter.js
+++ feb17/middleware/ratelimiter.js
@@ -4,7 +4,14 @@ const clientredis=require("./../config/redis")
 
 const ratelimiter=async (req,res,next)=>{
     try{
-
+        const ip=req.ip;
+        const cnt=await clientredis.incr(ip);
+        if(cnt==1){
+            await clientredis.expire(3600) //1 min
+        }
+        if(cnt>60){
+            throw new Error("Too many request")
+        }
     }
     catch(err){
         res.send("ERROR " + err.message)
