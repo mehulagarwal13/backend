6db3e55cb2017385e47e19cd255d355ceb6c85c7
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-02-26 11:25:39 +0530

    ip

--- feb17/middleware/ratelimiter.js
+++ feb17/middleware/ratelimiter.js
@@ -3,7 +3,12 @@ const user=require("../user")
 const clientredis=require("./../config/redis")
 
 const ratelimiter=async (req,res,next)=>{
+    try{
 
+    }
+    catch(err){
+        res.send("ERROR " + err.message)
+    }
 }
 
 module.exports=ratelimiter;
