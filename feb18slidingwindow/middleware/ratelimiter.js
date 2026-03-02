4621924a8a35778f34f84a43b3d4dbec6dc69618
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-03-02 16:00:35 +0530

    sliding window

--- feb18slidingwindow/middleware/ratelimiter.js
+++ feb18slidingwindow/middleware/ratelimiter.js
@@ -4,15 +4,8 @@ const clientredis=require("./../config/redis")
 
 const ratelimiter=async (req,res,next)=>{
     try{
-        const ip=req.ip;
-        const cnt=await clientredis.incr(ip);
-        if(cnt==1){
-            await clientredis.expire(3600) //1 min
-        }
-        if(cnt>10){
-            throw new Error("Too many request")
-        }
-        next();
+       const key=req.ip;
+       
     }
     catch(err){
         
