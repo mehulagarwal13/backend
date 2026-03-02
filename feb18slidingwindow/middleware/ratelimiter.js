995ec2aa0064f907d93dda1e8ceca9175ce4c7a7
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-03-02 16:03:39 +0530

    const request_count=await clientredis.zcard(key)

--- feb18slidingwindow/middleware/ratelimiter.js
+++ feb18slidingwindow/middleware/ratelimiter.js
@@ -7,7 +7,9 @@ const ratelimiter=async (req,res,next)=>{
        const key=req.ip;
        const current_time=Date.now()/1000; // Convert to seconds
        const request_time=current_time-3600; //isse phle ki request ko delete karna hoga
-    
+
+       await clientredis.zremrangebyscore(key,0,request_time) //isse phle ki request ko delete karna hoga
+       const request_count=await clientredis.zcard(key) //isse current request count milega
 
     }
     catch(err){
