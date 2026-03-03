57bbf85eef778f38b706922248794d6f6c1f1c8f
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-03-03 22:44:52 +0530

    hg

--- feb23/index.js
+++ feb23/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const user = require("./user");
 const validatoruser = require("./validator");
-const bcrypt = require("bcrypt");
-
-
 
 
 const app = express();
